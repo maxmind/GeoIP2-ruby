c0d6e16eb2c5eaf2ff1874d8d7873ee20a7ddc1e
Gregory Oschwald <goschwald@maxmind.com>
2025-12-03 10:02:45 -0800

    Support pre-release versions in release script

--- dev-bin/release.sh
+++ dev-bin/release.sh
@@ -54,7 +54,7 @@ fi
 changelog=$(cat CHANGELOG.md)
 
 regex='
-## ([0-9]+\.[0-9]+\.[0-9]+) \(([0-9]{4}-[0-9]{2}-[0-9]{2})\)
+## ([0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9.]+)?) \(([0-9]{4}-[0-9]{2}-[0-9]{2})\)
 
 ((.|
 )*)
@@ -66,8 +66,8 @@ if [[ ! $changelog =~ $regex ]]; then
 fi
 
 version="${BASH_REMATCH[1]}"
-date="${BASH_REMATCH[2]}"
-notes="$(echo "${BASH_REMATCH[3]}" | sed -n -E '/^## [0-9]+\.[0-9]+\.[0-9]+/,$!p')"
+date="${BASH_REMATCH[3]}"
+notes="$(echo "${BASH_REMATCH[4]}" | sed -n -E '/^## [0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9.]+)?/,$!p')"
 
 echo "$notes"
 if [[ "$date" != "$(date +"%Y-%m-%d")" ]]; then
